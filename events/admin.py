082e0710bd40933bd433df1def44495758abb6dc
alexymleung <aymleung@gmail.com>
2025-01-18 11:00:09 +0800

    media completed 3rd commit

--- events/admin.py
+++ events/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from .models import Event
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('id','event_type','title','publish_date')
+    list_display = ('id','event_type','title', 'photo_main', 'photo_1', 'photo_2', 'photo_3', 'photo_4', 'publish_date')
     list_display_links = ('id','title')
     list_filter = 'event_type',
     search_fields = 'title','content'
